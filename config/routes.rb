2e0fb5b1b379da26488a7790f84ceb5dcc05928b
SatnamSingh <satnam.ss940@gmail.com>
2020-07-11 15:25:41 +0530

    api created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :authors
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	root 'pages#home'
+	get 'about', to: 'pages#about'
+	namespace :api do
+	  namespace :v1 do
+	    resources :books
+	    resources :authors
+	  end
+  end 
 end
