a9bad603ec93dc44ec639c67a23db847d7facb2d
SatnamSingh <satnam.ss940@gmail.com>
2020-07-15 14:51:54 +0530

    remain work

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -1,74 +1,18 @@
-class Api::V1::AuthorsController < ApplicationController
-  before_action :set_author, only: [:show, :edit, :update, :destroy]
-
-  # GET /authors
-  # GET /authors.json
-  def index
-    @authors = Author.all
-  end
-
-  # GET /authors/1
-  # GET /authors/1.json
-  def show
-  end
-
-  # GET /authors/new
-  def new
-    @author = Author.new
-  end
-
-  # GET /authors/1/edit
-  def edit
-  end
-
-  # POST /authors
-  # POST /authors.json
-  def create
-    @author = Author.new(author_params)
-
-    respond_to do |format|
-      if @author.save
-        format.html { redirect_to @author, notice: 'Author was successfully created.' }
-        format.json { render :show, status: :created, location: @author }
-      else
-        format.html { render :new }
-        format.json { render json: @author.errors, status: :unprocessable_entity }
+module Api
+  module V1
+    class AuthorsController < ApplicationController
+      def index
+        authors_books = [[]]
+        authors = []
+        books = []
+        authors = Author.all
+        authors.each do |author|
+          books << author.books
+          authors << author
+          authors_books << [authors][books] 
+        end
+        render json: {status: 'SUCCESS', message:'Loaded authors', data:authors_books},status: :ok
       end
     end
   end
-
-  # PATCH/PUT /authors/1
-  # PATCH/PUT /authors/1.json
-  def update
-    respond_to do |format|
-      if @author.update(author_params)
-        format.html { redirect_to @author, notice: 'Author was successfully updated.' }
-        format.json { render :show, status: :ok, location: @author }
-      else
-        format.html { render :edit }
-        format.json { render json: @author.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /authors/1
-  # DELETE /authors/1.json
-  def destroy
-    @author.destroy
-    respond_to do |format|
-      format.html { redirect_to authors_url, notice: 'Author was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def author_params
-      params.require(:author).permit(:name, :age, book_ids:[])
-    end
 end
