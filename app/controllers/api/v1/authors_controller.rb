2cd9328df9cf5ea13ced8f6825180a64d6fd7cc7
SatnamSingh <satnam.ss940@gmail.com>
2020-07-15 18:54:07 +0530

    Changes made for api and some issue fixed

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -2,16 +2,8 @@ module Api
   module V1
     class AuthorsController < ApplicationController
       def index
-        authors_books = [[]]
-        authors = []
-        books = []
         authors = Author.all
-        authors.each do |author|
-          books << author.books
-          authors << author
-          authors_books << [authors][books] 
-        end
-        render json: {status: 'SUCCESS', message:'Loaded authors', data:authors_books},status: :ok
+        render json: {status: 'SUCCESS', message:'Loaded authors', data:authors},status: :ok
       end
     end
   end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
  has_and_belongs_to_many :books
+ accepts_nested_attributes_for :books
 end
