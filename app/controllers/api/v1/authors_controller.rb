8bc0c94450ee4966667bdd1dab5abddfe759fb04
SatnamSingh <satnam.ss940@gmail.com>
2020-07-16 19:07:37 +0530

    issues fixed and completed assignment

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -2,8 +2,13 @@ module Api
   module V1
     class AuthorsController < ApplicationController
       def index
-        authors = Author.all
+        authors = Author.all.as_json(include: :books)
+        # authors = Author.all
         render json: {status: 'SUCCESS', message:'Loaded authors', data:authors},status: :ok
+      end
+      def show
+        author = Author.find(params[:id]).as_json(include: :books)
+        render json: {status: 'SUCCESS', message:'Loaded authors', data:author},status: :ok
       end
     end
   end
--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -5,6 +5,10 @@ module Api
         books = Book.all
         render json: {status: 'SUCCESS', message:'Loaded books', data:books},status: :ok
       end
+      def show
+        book = Book.find(params[:id])
+        render json: {status: 'SUCCESS', message:'Loaded authors', data:book},status: :ok
+      end
     end
   end
 end
